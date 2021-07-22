15a68d66097e289dead3c3f006d4a7cfeb4ae472
vntu <vntu98@gmail.com>
2021-07-22 20:23:51 +0700

    handle logout

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/login', LoginController::class);
+Route::post('/logout', LogoutController::class);
