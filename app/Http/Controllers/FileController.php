3cb08e1b6896e28d384cea8487acfeca5f42369d
vntu <vntu98@gmail.com>
2021-07-23 21:43:51 +0700

    setup s3 env

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\FileResource;
+use Aws\S3\PostObjectV4;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class FileController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth:sanctum']);
+    }
+
+    public function index(Request $request)
+    {
+        return FileResource::collection($request->user()->files);
+    }
+    
+    public function signed(Request $request)
+    {
+        $filename = md5($request->name . microtime()) . '.' . $request->extension;
+        
+        $object = new PostObjectV4(
+            Storage::disk('s3')->getAdapter()->getClient(),
+            config('filesystems.disks.s3.bucket'),
+            ['key' => 'files/' . $filename],
+            [
+                ['bucket' => config('filesystems.disks.s3.bucket')],
+                ['starts-with', '$key', 'files/']
+            ]
+        );
+
+        return response()->json([
+            'attributes' => $object->getFormAttributes(),
+            'additionalData' => $object->getFormInputs()
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login', LoginController::class);
 Route::post('/logout', LogoutController::class);
+
+Route::get('/files', [FileController::class, 'index']);
+Route::post('/files/signed', [FileController::class, 'signed']);
